538f885c98d5eeda10909f1612e88004f949bc48
Isnawati <isnaawt19@gmail.com>
2026-09-23 19:12:46 +0800

    Tambah kelas KartuMahasiswa PBO 2

---
+++ p01-halo-java/src/main/java/id/ac/uniska/pbo2/p01/KartuMahasiswa.java
@@ -0,0 +1,23 @@
+package id.ac.uniska.pbo2.p01;
+
+public class KartuMahasiswa {
+
+    public static void main(String[] args) {
+
+        String nama = "Isnawati";
+        String npm = "2410010336";
+        String prodi = "Teknik Informatika";
+        int semester = 5;
+        String alasan = "Ingin memahami pemrograman berorientasi objek dan membuat aplikasi";
+
+        System.out.println("==============================");
+        System.out.println("KARTU MAHASISWA PBO 2");
+        System.out.println("==============================");
+        System.out.println("Nama     : " + nama);
+        System.out.println("NPM      : " + npm);
+        System.out.println("Prodi    : " + prodi);
+        System.out.println("Semester : " + semester);
+        System.out.println("Alasan   : " + alasan);
+        System.out.println("==============================");
+    }
+}
